1eaaed4f41bbc83e2ba8e533c0b816e7d94c5438
Hiba Machfej <hiba.mashfij@gmail.com>
2020-07-21 12:29:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// Write your code here!
+let element = document.querySelector('main#main');
+
+element.remove()
+
+
+
+
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
